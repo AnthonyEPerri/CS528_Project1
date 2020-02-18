11be6d29b5d3ad9c0281506501b86472f5487947
anperri <anthonyeperri@gmail.com>
2020-02-17 22:01:25 -0600

    added menu, day/night shift, teleporter

---
+++ Assets/custom_menu_options.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class custom_menu_options : MonoBehaviour
+{
+    public GameObject day_assets;
+    public GameObject night_assets;
+    public GameObject day_cam;
+    public GameObject night_cam;
+    public GameObject playerPos;
+
+    public GameObject day_btn;
+    public GameObject night_btn;
+    public GameObject entrance_area_btn;
+    public GameObject rest_area_btn;
+    public GameObject back_area_btn;
+    public GameObject main_area_btn;
+
+    public GameObject entrance_area_mkr;
+    public GameObject rest_area_mkr;
+    public GameObject back_area_mkr;
+    public GameObject main_area_mkr;
+
+
+    public void click_day()
+    {
+        day_assets.SetActive(true);
+        day_cam.SetActive(true);
+        night_assets.SetActive(false);
+        night_cam.SetActive(false);
+    }
+
+    public void click_night()
+    {
+        day_assets.SetActive(false);
+        day_cam.SetActive(false);
+        night_assets.SetActive(true);
+        night_cam.SetActive(true);
+    }
+
+    public void click_main_area()
+    {
+        playerPos.transform.position = main_area_mkr.transform.position;
+    }
+
+    public void click_back_area()
+    {
+        playerPos.transform.position = back_area_mkr.transform.position;
+    }
+
+    public void click_rest_area()
+    {
+        playerPos.transform.position = rest_area_mkr.transform.position;
+    }
+
+    public void click_entrance_area()
+    {
+        playerPos.transform.position = entrance_area_mkr.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
